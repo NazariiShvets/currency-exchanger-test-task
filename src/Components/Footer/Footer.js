c849bdd7d8869222f3edae2ade27e62885555a5b
Nazarii <nazarii.shvets@gmail.com>
2020-12-25 12:33:37 +0200

    init project

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import './Footer.scss'
+
+
+const mapStateToProps = (state) => ({
+    socialLinks: state.footer.socialLinks
+})
+const Footer = ({ socialLinks }) => (
+    <footer className="footer">
+        <div className='footer__title'> Created by Nazarii Shvets</div>
+        <div className='footer__links footer-links'>
+            {socialLinks.map(link => (
+                <a className='footer-links__link' key={link.id} href={link.url} target='_blank'
+                    rel='noreferrer'>
+                    <img src={link.logo} key={link.id} alt="" width='30px' className='footer-links__img' />
+                </a>
+            ))}
+        </div>
+    </footer>
+)
+
+
+export default connect(mapStateToProps, {})(Footer)
