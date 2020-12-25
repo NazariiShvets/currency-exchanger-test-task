de0ba1639e6a24b413660713adf35e190b5259b1
Nazarii <nazarii.shvets@gmail.com>
2020-12-25 12:59:58 +0200

    first probs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import './App.css'
+import Footer from 'Footer'
+import './index.scss'
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Hello</h1>
+      <Footer/>
     </div>
   );
 }
