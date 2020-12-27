57bd588af262b3338aca22d7669a232ccb3c8aa0
Nazarii <nazarii.shvets@gmail.com>
2020-12-27 23:34:13 +0200

    last commit

--- src/Components/Converter/Converter.js
+++ src/Components/Converter/Converter.js
@@ -16,22 +16,24 @@ const Converter = ({
                        setInitialStateToExchanges,
                        getExchanges,
                        basicRate,
-                       setBasicRate,
-                       ...props
+                       setBasicRate
                    }) => {
     const [amount, setAmount] = useState(0)
     const firstSelectRef = React.createRef()
     const secondSelectRef = React.createRef()
     const firstInputRef = React.createRef()
+
     useEffect(() => {
         getExchanges()
     }, [getExchanges, basicRate])
     useEffect(() => () => {
         setInitialStateToExchanges()
     }, [setInitialStateToExchanges])
+
     if (isExchangesFetching) {
         return <Loader/>
     }
+
     const inputHandler = () => {
         setAmount(Math.trunc(+firstInputRef.current.value * +firstSelectRef.current.value / +secondSelectRef.current.value * 100) / 100)
     }
@@ -39,69 +41,69 @@ const Converter = ({
         setBasicRate(JSON.parse(event.target.value))
     }
     const firstSelectRatesWithBasicRate = exchanges.reduce((arr, rate) => arr.some(exchange => exchange.r030 === rate.r030)
-        ? arr : [...arr, rate], [basicRate]).map(exchange => <option key={exchange.r030}
-                                                                     value={exchange.rate}>{exchange.txt}</option>)
+        ? arr
+        : [...arr, rate], [basicRate]).map(exchange => <option key={exchange.r030}
+                                                               value={exchange.rate}>{exchange.txt}</option>)
     const secondSelectRatesWithBasicRate = exchanges.reduce((arr, rate) => arr.some(exchange => exchange.r030 === rate.r030)
-        ? arr : [...arr, rate], [basicRate]).map(exchange => <option key={exchange.r030}
-                                                                     value={exchange.rate}>{exchange.txt}</option>)
-    return (
-        <div className='container mt1'>
-            <div className='row mb2'>
-                <div className='col-sm'>Базова валюта : {basicRate.txt}</div>
-            </div>
-            <div className='row'>
-                <div className='col-sm mb2'>
-                    Поміняти базову валюту
-                    <select onChange={basicRateHandler} className="form-select mt1">
-                        {exchanges.map(exchange => <option
-                            key={exchange.r030}
-                            value={JSON.stringify(exchange)}
-                        >
-                            {exchange.txt}
-                        </option>)}
-                    </select>
-                </div>
-                <hr/>
-            </div>
-            <div className="row">
-                <div className="col-sm">
-                    <select ref={firstSelectRef}
-                            onChange={inputHandler}
-                            className="form-select"
-                            aria-label="Default select example">
-                        {firstSelectRatesWithBasicRate}
-                    </select>
-                </div>
-                <div className="col-sm">
-                    <input type="number"
-                           step="0.01"
-                           min="0"
-                           ref={firstInputRef}
-                           onChange={inputHandler}
-                           required
-                           className="form-control"
-                           placeholder="Введите значение"
-                    />
-                </div>
-                <div className="col-sm">
-                    <select ref={secondSelectRef}
-                            onChange={inputHandler}
-                            className="form-select"
-                            aria-label="Default select example"
+        ? arr
+        : [...arr, rate], [basicRate]).map(exchange => <option key={exchange.r030}
+                                                               value={exchange.rate}>{exchange.txt}</option>)
+    return (<div className='container mt1'>
+        <div className='row mb2'>
+            <div className='col-sm'>Базова валюта : {basicRate.txt}</div>
+        </div>
+        <div className='row'>
+            <div className='col-sm mb2'>
+                Поміняти базову валюту
+                <select onChange={basicRateHandler} className="form-select mt1">
+                    {exchanges.map(exchange => <option
+                        key={exchange.r030}
+                        value={JSON.stringify(exchange)}
                     >
-                        {secondSelectRatesWithBasicRate}
-                    </select>
-                </div>
-                <div className="col-sm">
-                    <input type="number"
-                           value={amount}
-                           disabled
-                           className="form-control"
-                           placeholder="Result"
-                    />
-                </div>
+                        {exchange.txt}
+                    </option>)}
+                </select>
+            </div>
+            <hr/>
+        </div>
+        <div className="row">
+            <div className="col-sm">
+                <select ref={firstSelectRef}
+                        onChange={inputHandler}
+                        className="form-select"
+                >
+                    {firstSelectRatesWithBasicRate}
+                </select>
+            </div>
+            <div className="col-sm">
+                <input type="number"
+                       step="0.01"
+                       min="0"
+                       ref={firstInputRef}
+                       onChange={inputHandler}
+                       className="form-control"
+                       placeholder="Enter amount"
+                />
+            </div>
+            <div className="col-sm">
+                <select ref={secondSelectRef}
+                        onChange={inputHandler}
+                        className="form-select"
+                        aria-label="Default select example"
+                >
+                    {secondSelectRatesWithBasicRate}
+                </select>
+            </div>
+            <div className="col-sm">
+                <input type="number"
+                       value={amount}
+                       disabled
+                       className="form-control"
+                       placeholder="Result"
+                />
             </div>
         </div>
-    )
+    </div>)
 }
+
 export default connect(mapStateToProps, {getExchanges, setInitialStateToExchanges, setBasicRate})(Converter)
